12a8487348e086be3503a7e11d294c6267feeea7
Jacqueline <jacqueline@ordrstudio.com>
2018-04-04 18:32:06 -0400

    Server running but no check for other things

--- proj.h
+++ proj.h
@@ -5,12 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_DATA_SIZE 1024
 #define CMD_SEND 0
 #define CMD_RECV 1
 #define CMD_RESP 2
 #define CMD_DATA 3
+
+#define CLIENT 0
+#define SERVER 1
 
 struct send_msg
 {
